2dc793232c4869d3ee8841c17c4ced7e7d7091a8
Bruno Guerra <gguerrabruno@outlook.com>
2020-03-22 23:59:14 -0300

    Version 1.0. Features implemented, needs revision and cleaning code

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 document.getElementById("submit-button").addEventListener("click", () => {
     const dataJson = getDataFromAPI()
     dataJson.then(data => {
-        updateWebsite(data)
+        data === false ? alert("No results found!") : updateWebsite(data)
     })
 })
 
@@ -11,18 +11,22 @@ async function getDataFromAPI(){
 
         const username = usernameField.value.trim()
 
-        const response = await fetch('https://api.github.com/users/'+username)
+        const mainDataResponse = await fetch('https://api.github.com/users/'+username)
 
-        if(response.status !== 200){
+        const reposResponse = await fetch('https://api.github.com/users/'+username+"/repos")
+
+        if(mainDataResponse.status !== 200 || reposResponse.status !== 200){
             throw "Request resulted 404."
         }
 
-        const data = await response.json()
+        const mainData = await mainDataResponse.json()
+        const reposData = await reposResponse.json();
         
-        return data
+        return {mainData: mainData, reposData: reposData}
     }
     catch(e){
         console.log("Error: " + e)
+        return false
     }
     finally{
         console.log("End of API request.")
@@ -30,10 +34,70 @@ async function getDataFromAPI(){
 }
 
 function updateWebsite(data){
-    console.log(data);
+
     document.getElementsByClassName("primary-data")[0].style.visibility="visible"
-    let fullname = document.getElementById("fullname")
-    let username = document.getElementById("username")
-    fullname.innerHTML = fullname.innerHTML + data.name
-    username.innerHTML = username.innerHTML + data.login
+
+    const fullname = document.getElementById("fullname")
+    const username = document.getElementById("username")
+    const avatar = document.createElement("img")
+    const repos = document.getElementById("repos")
+    const followers = document.getElementById("followers")
+    const reposContainer = document.getElementsByClassName("repos-container")[0]
+
+    document.getElementsByClassName("photo-container")[0].innerHTML=""
+
+    avatar.src=data.mainData.avatar_url
+    avatar.style="width: 80%;"
+
+    document.getElementsByClassName("photo-container")[0].appendChild(avatar)
+
+    fullname.innerHTML = "Fullname: " + data.mainData.name
+    username.innerHTML = "Username: " + data.mainData.login
+    repos.innerHTML = "Repos: " + data.mainData.public_repos
+    followers.innerHTML = "Followers: " + data.mainData.follower
+
+    reposContainer.innerHTML="";
+
+    const reposTitle = document.createElement("p")
+
+    if(data.mainData.public_repos == 0){
+        reposTitle.innerHTML = "User has 0 repos."
+        reposContainer.appendChild(reposTitle)
+    }
+    else{
+        reposTitle.innerHTML = "User repos: "
+        reposContainer.appendChild(reposTitle)
+
+        const reposArray = sortAndRetrieveRepos(data.reposData)
+
+        reposArray.forEach(elem => {
+            const link = document.createElement("a")
+            link.text = elem.name
+            link.href = elem.svn_url
+            reposContainer.appendChild(link)
+        })
+    }
+
+}
+
+function sortAndRetrieveRepos(repos){
+    try{
+        if(!repos){
+            throw "No repos from the user."
+        }
+        repos.sort((a,b) => {
+            if(a.created_at < b.created_at){
+                return 1
+            }
+            if(a.created_at > b.created_at){
+                return -1
+            }
+            return 0
+        })
+        return repos.slice(0,4);
+    }
+    catch(e){
+        console.log("Error: " + e)
+        return false
+    }
 }
