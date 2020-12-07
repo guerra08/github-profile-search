904bf4fa935deb6b165b84c0889f3379cc785ae9
Bruno Guerra <gguerrabruno@outlook.com>
2020-12-06 21:35:40 -0300

    feat
    Better performance for requests; using promises.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 submitButton.addEventListener("click", () => {
-    executeBulk()
+    executeBulk();
 })
 
 usernameField.addEventListener("keyup", (e) => {
@@ -11,107 +11,95 @@ darkToggle.addEventListener("click", () => {
 })
 
 function executeBulk(){
-    const dataJson = getDataFromAPI()
-    dataJson.then(data => {
-        data !== false ? updateWebsite(data) : {}
-    })
+    getDataFromAPI()
+        .then(data => {
+            (data) ? updateWebsite(data) : alert("User not found");
+        });
 }
 
 function darkOn(){
-    themeName.innerHTML = "🌑"
-    darkToggle.value = "D"
+    themeName.innerHTML = "🌑";
+    darkToggle.value = "D";
     root.style.setProperty('--bg', "#2B2B2D");
     root.style.setProperty('--color', "#E4E4E4");
 }
 
 function lightOn(){
-    themeName.innerHTML = "☀️"
-    darkToggle.value = "L"
+    themeName.innerHTML = "☀️";
+    darkToggle.value = "L";
     root.style.setProperty('--color', "#2B2B2D");
     root.style.setProperty('--bg', "#E4E4E4");
 }
 
-async function getDataFromAPI(){
-    try{
+function getDataFromAPI(){
+    checkUsername();
 
-        checkUsername()
+    const username = usernameField.value.trim();
 
-        const username = usernameField.value.trim()
+    usernameField.value = '';
 
-        usernameField.value = ''
+    const mainDataResponse = fetch(`https://api.github.com/users/${username}`, {
+        mode: 'cors'
+    });
+    const reposResponse = fetch(`https://api.github.com/users/${username}/repos`, {
+        mode: 'cors'
+    });
 
-        const mainDataResponse = await fetch(`https://api.github.com/users/${username}`, {
-            mode: 'cors'
+    return Promise.all([mainDataResponse, reposResponse])
+        .then(values => {
+            const pendingJson = values.filter(e => e.status === 200).map(e => e.json());
+            return Promise.all(pendingJson)
+                .then(completedJson => {
+                    return (completedJson.length == 2) ? { mainData: completedJson[0], reposData: completedJson[1] } : false;
+                })
         })
-
-        const reposResponse = await fetch(`https://api.github.com/users/${username}/repos`, {
-            mode: 'cors'
-        })
-
-        if(mainDataResponse.status !== 200 || reposResponse.status !== 200){
-            alert("No results found!")
-            throw "Error while processing request."
-        }
-
-        const mainData = await mainDataResponse.json()
-        const reposData = await reposResponse.json();
-        
-        return {mainData: mainData, reposData: reposData}
-    }
-    catch(e){
-        console.log(`Error: ${e}`)
-        return false;
-    }
-    finally{
-        console.log("End of API request.");
-    }
 }
 
 function updateWebsite(data){
 
-    document.getElementsByClassName("primary-data")[0].style.visibility="visible"
+    document.getElementsByClassName("primary-data")[0].style.visibility="visible";
 
     const fullname = document.getElementById("fullname");
     const username = document.getElementById("username");
     const avatar = document.createElement("img");
     const repos = document.getElementById("repos");
     const followers = document.getElementById("followers");
-    const reposContainer = document.getElementsByClassName("repos-container")[0]
+    const reposContainer = document.getElementsByClassName("repos-container")[0];
 
-    document.getElementsByClassName("photo-container")[0].innerHTML = ''
+    document.getElementsByClassName("photo-container")[0].innerHTML = '';
 
-    avatar.src=data.mainData.avatar_url
-    avatar.style="width: 65%; border: 5px solid #021a40;"
+    avatar.src=data.mainData.avatar_url;
+    avatar.style="width: 65%; border: 5px solid #021a40;";
 
-    document.getElementsByClassName("photo-container")[0].appendChild(avatar)
+    document.getElementsByClassName("photo-container")[0].appendChild(avatar);
 
-    fullname.innerHTML = `Fullname: ${data.mainData.name}`
-    username.innerHTML = `Username: ${data.mainData.login}`
-    repos.innerHTML = `Repos: ${data.mainData.public_repos}`
-    followers.innerHTML = `Followers: ${data.mainData.followers}`
+    fullname.innerHTML = `Fullname: ${data.mainData.name}`;
+    username.innerHTML = `Username: ${data.mainData.login}`;
+    repos.innerHTML = `Repos: ${data.mainData.public_repos}`;
+    followers.innerHTML = `Followers: ${data.mainData.followers}`;
 
     reposContainer.innerHTML= '';
 
-    const reposTitle = document.createElement("p")
+    const reposTitle = document.createElement("p");
 
     if(data.mainData.public_repos == 0){
-        reposTitle.innerHTML = 'User has 0 public repos.'
-        reposContainer.appendChild(reposTitle)
+        reposTitle.innerHTML = 'User has 0 public repos.';
+        reposContainer.appendChild(reposTitle);
     }
     else{
-        reposTitle.innerHTML = 'User repos: '
-        reposContainer.appendChild(reposTitle)
+        reposTitle.innerHTML = 'User repos: ';
+        reposContainer.appendChild(reposTitle);
 
-        const reposArray = sortAndRetrieveRepos(data.reposData)
+        const reposArray = sortAndRetrieveRepos(data.reposData);
 
         reposArray.forEach(elem => {
-            const label = document.createElement("label")
-            const link = document.createElement("a")
-            label.textContent = elem.description
-            link.text = elem.name
-            link.href = elem.svn_url
-            reposContainer.appendChild(link)
-            reposContainer.appendChild(label)
+            const label = document.createElement("label");
+            const link = document.createElement("a");
+            label.textContent = elem.description;
+            link.text = elem.name;
+            link.href = elem.svn_url;
+            reposContainer.appendChild(link);
+            reposContainer.appendChild(label);
         })
     }
 
@@ -120,28 +108,28 @@ function updateWebsite(data){
 function sortAndRetrieveRepos(repos){
     try{
         if(!repos){
-            throw "No repos from the user."
+            throw "No repos from the user.";
         }
         repos.sort((a,b) => {
             if(a.created_at < b.created_at){
-                return 1
+                return 1;
             }
             if(a.created_at > b.created_at){
-                return -1
+                return -1;
             }
             return 0;
         })
         return repos.slice(0,4);
     }
     catch(e){
-        console.log(`Error: ${e}`)
+        console.log(`Error: ${e}`);
         return false;
     }
 }
 
 function checkUsername(){
     if(usernameField.value === ''){
-        alert("Empty username!")
-        throw "Empty username!"
+        alert("Empty username!");
+        throw "Empty username!";
     }    
 }
